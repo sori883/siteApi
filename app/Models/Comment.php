d92a9789490d07e2ca465b94a19922987b519fd6
sori883 <service@dlme.biz>
2021-11-17 23:11:15 +0900

    models下地作成

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = [
+        'deleted_at',
+        'publish_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'comment_entry',
+        'publish_at',
+    ];
+
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\Article');
+    }
 }
