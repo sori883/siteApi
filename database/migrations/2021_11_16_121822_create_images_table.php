6fb7b3243bcc47ed9f0541d40197e33eee81e653
sori883 <service@dlme.biz>
2021-11-17 23:10:44 +0900

    不足部部追記

--- database/migrations/2021_11_16_121822_create_images_table.php
+++ database/migrations/2021_11_16_121822_create_images_table.php
@@ -15,10 +15,13 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable()->default(null)->comment('投稿者のID');
             $table->string('title', 255)->unique()->default('')->comment('画像のタイトル');
             $table->string('path', 255)->unique()->default('')->comment('画像の保存パス');
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
