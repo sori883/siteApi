5a34db9b8e3516e0b5d2137caaaaf3f7b27f54dc
sori883 <service@dlme.biz>
2022-01-15 14:11:10 +0900

    サジェスト用のタグ一覧と記事保存用のURL追加

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\MeController;
 use App\Http\Controllers\Auth\ForgotController;
 use App\Http\Controllers\Auth\PasswordResetController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +21,8 @@ use App\Http\Controllers\Auth\PasswordResetController;
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/me', MeController::class);
+    Route::get('/fetchAllTags', [TagController::class, 'fetchAllTags']);
+    Route::post('/storeArticle', [ArticleController::class, 'store']);
 });
 
 Route::post('/forgot', [ForgotController::class, 'forgot']);
