c06d68625fc6b875fc6b48bc2768f44dfa74bbfc
sori883 <service@dlme.biz>
2022-01-24 06:18:12 +0900

    リクエストユーザに紐づくものだけ取得するように変更

--- app/UseCase/Article/FetchAllArticleAction.php
+++ app/UseCase/Article/FetchAllArticleAction.php
@@ -3,13 +3,16 @@
 namespace App\UseCase\Article;
 
 use App\Models\Article;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 
 class FetchAllArticleAction
 {
-    public function __invoke(): Collection
+    public function __invoke(User $user): Collection
     {
-        $articles = Article::select('id', 'title', 'permalink', 'publish_at')->get();
+        $articles = Article::select('id', 'title', 'permalink', 'publish_at')
+        ->where('user_id', $user->id)
+        ->get();
         return $articles;
     }
 }
