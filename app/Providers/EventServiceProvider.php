2b2a2e66a7054a6bbbff282280de3a596d0103ef
sori883 <service@dlme.biz>
2022-02-26 19:06:15 +0900

    更新時にキャッシュを削除するためにObserver入れた

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,12 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Observers\ArticleObserver;
+use App\Models\Article;
+use App\Observers\CategoryObserver;
+use App\Models\Category;
+use App\Observers\TagObserver;
+use App\Models\Tag;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -27,6 +33,9 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // キャッシュ削除用のObserver登録
+        Article::observe(ArticleObserver::class);
+        Category::observe(CategoryObserver::class);
+        Tag::observe(TagObserver::class);
     }
 }
